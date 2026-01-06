596bd819bcfdc959f9b75d46616bb788755a0fec
Anshuman <anshumantyagi20@gmail.com>
2026-01-07 01:41:04 +0530

    fixing App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Home from './pages/Home'; // Ensure this path matches where you placed Home.jsx
+import Home from './Pages/Home'; // Ensure this path matches where you placed Home.jsx
 
 function App() {
   return (
