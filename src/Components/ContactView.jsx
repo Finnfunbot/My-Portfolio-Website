78fabfff6e3acc8250d7b25aa75143f5d415c625
Finnfunbot <gregory@simonefamily.net>
2026-01-13 03:17:15 -0500

    Update contact message for engineering internship inquiry

--- src/Components/ContactView.jsx
+++ src/Components/ContactView.jsx
@@ -20,8 +20,7 @@ export default function ContactView() {
             Let's Work Together
           </h1>
           <p className="text-[#00416B]/70 max-w-2xl mx-auto text-lg">
-            Have a challenging engineering problem? I'd love to hear about it. 
-            Reach out and let's discuss how I can help bring your ideas to life.
+            Are you looking for an engineer intern? I'd love to hear about it!
           </p>
         </motion.div>
 
